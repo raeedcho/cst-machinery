a94e5b00fb5822ae1993c25d30c91dc9b794fb7a
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2025-04-05 17:54:30 -0400

    feat: finish train_lfads stage with saving model

--- scripts/train_lfads.py
+++ scripts/train_lfads.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from pathlib import Path
 
 import logging
+logger = logging.getLogger(__name__)
+
 import os
 import warnings
 from glob import glob
@@ -26,23 +28,26 @@ def main(args):
     log_dir = Path(args.logdir) / 'train-lfads'
     if not log_dir.exists():
         log_dir.mkdir(parents=True, exist_ok=True)
-    
     logging.basicConfig(
         filename=log_dir / f'{dataset_str}.log',
         level=args.loglevel,
     )
 
-    # os.chdir(run_dir)
-    train_model(
+    os.chdir(run_dir)
+    model = train_model(
         config_path=args.config,
         # checkpoint_dir='./lightning_checkpoints',
     )
+
+    # Save the model
+    torch.save(model.state_dict(), "lfads_model.pt")
+    logger.info(f'Saved model as lfads_model.pt')
 
 def train_model(
     overrides: dict = {},
     config_path: str = None,
     checkpoint_dir: str = None,
-):
+) -> pl.LightningModule:
     # load config
     config_path = Path(config_path)
     overrides = [f"{k}={v}" for k, v in flatten(overrides).items()]
@@ -92,6 +97,8 @@ def train_model(
     if config.posterior_sampling.use_best_ckpt:
         ckpt_path = trainer.checkpoint_callback.best_model_path
         model.load_state_dict(torch.load(ckpt_path)["state_dict"])
+
+    return model
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract and save trial frame from SMILE data')
