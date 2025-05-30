e7e72382c323ed1e882b3d15c87ad8472f2d8ed1
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2025-05-30 16:04:57 -0400

    fix: rename targets for data extraction

--- scripts/extract_normalized_data.py
+++ scripts/extract_normalized_data.py
@@ -103,11 +103,20 @@ def save_binned_spikes(smile_data_blocks: dict[str, list[Any]], output_path: Pat
     logger.info(f'Saved binned spikes to {output_path}')
 
 def save_targets(smile_data_blocks: dict[str, list[Any]], output_path: Path) -> None:
+    target_mapper = {
+        'starttarget': 'start',
+        'tasktarget': 'task cue',
+        'reachtarget': 'reach',
+    }
     targets = smile_extract.concat_block_trial_func_results(
         smile_extract.get_trial_targets,
         smile_data_blocks,
     )
-    targets.to_parquet(output_path)
+    (
+        targets
+        .rename(index=target_mapper, level='target')
+        .to_parquet(output_path)
+    )
     logger.info(f'Saved targets to {output_path}')
 
 if __name__ == '__main__':
